b0401b6476408749179306f8559dc33272638425
mehulgondaliya <mehulgondaliya@knovator.in>
2024-03-28 16:02:41 +0530

    post method with api routes api

--- app/api/users/route.js
+++ app/api/users/route.js
@@ -1,6 +1,12 @@
 import { userDetails } from "@/app/utility/db";
 import { NextResponse } from "next/server";
 
-export function GET(request){
+export function GET(){
     return NextResponse.json(userDetails,{status:200})
+}
+
+export async function POST(request){
+    let payload = await request.json()
+    console.log(payload)
+    return NextResponse.json({result:'hello'})
 }
